112d47c0dec59cdb24449fc41a99a9a59471eab7
Abhigyan Paul <63092312+AbhigyanPaul@users.noreply.github.com>
2020-10-18 14:51:48 +0530

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,6 @@
 const navlink = document.querySelector(".navlinks");
 const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    navlink.classList.toggle("active");
+});
